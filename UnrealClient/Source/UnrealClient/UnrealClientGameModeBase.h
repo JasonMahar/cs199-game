dccca9cfbe7c17d3d19f3bff3fd1957e323c794d
Jason Mahar <jasonmahar@gmail.com>
2022-10-16 19:32:59 -0700

    adding empty UE5 client with menus

---
+++ UnrealClient/Source/UnrealClient/UnrealClientGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealClientGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALCLIENT_API AUnrealClientGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
